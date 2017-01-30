e9d5f5a03cce7ec65412fae258f42fbeba9d1301
Louis-David JAKUBOWICZ <ld.jakubowicz@gmail.com>
2017-01-30 16:02:39 +0100

    car page second step

--- client/src/actions/car.js
+++ client/src/actions/car.js
@@ -1,8 +1,31 @@
-import * as types from '../constants/car';
+import * as typesCar from '../constants/car';
 
-export function getCarsDB() {
+export function getCar(id) {
     return {
-        type: types.GET_CARS
+        type: typesCar.GET_CAR,
+        id
     };
 }
 
+export function addCar(id) {
+    return {
+        type: typesCar.ADD_CAR,
+        id
+    };
+}
+
+export function patchCar(name, newName) {
+    alert("ACTION --> PATCH_CAR: " + name);
+    return {
+        type: typesCar.PATCH_CAR,
+        name,
+        newName
+    };
+}
+
+export function deleteCar(id) {
+    return {
+        type: typesCar.DELETE_CAR,
+        id
+    };
+}
